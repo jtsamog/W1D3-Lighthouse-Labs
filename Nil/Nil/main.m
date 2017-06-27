462b33a61efb5f66552f94d5a6784958ee9cbe07
steve thompson <steven.thompson.ca@gmail.com>
2017-06-27 16:42:52 -0400

    Updates to nil demo

--- Nil/Nil/main.m
+++ Nil/Nil/main.m
@@ -9,12 +9,12 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-
+  
   // You can send a message to nil in Objc unlike most languages
   NSNumber *number;
   NSInteger integer = number.integerValue;
   NSLog(@"line %d: %ld", __LINE__, (long)integer);
-  NSString *string;
+  NSString *string; // defaults to nil
   NSString *upper = string.uppercaseString;
   NSLog(@"line %d: %@", __LINE__, upper);
   
@@ -26,9 +26,37 @@ int main(int argc, const char * argv[]) {
   
   NSLog(@"line %d: %@", __LINE__, string);
   
+  if (nil == 0) {
+    NSLog(@"line %d: %@", __LINE__, @"nil evaluates to 0");
+  }
+  
+  if (!nil) {
+    NSLog(@"line %d: %@", __LINE__, @"nil evaluates to false");
+  }
+  
+  // NSNull is an object wrapper for nil
   NSArray *array = @[[NSNull null]];
   NSNull *null = array[0];
   NSLog(@"line %d: %@", __LINE__, null);
+  
+  // Nil Rarely used
+  Class someClass = Nil;
+  someClass = [NSString class];
+  if (someClass == [NSString class]) {
+    NSLog(@"line: %d %@",__LINE__, @"Yes it is!" );
+  }
+  
+  // NULL is for C pointers
+  int *pInt = NULL;
+  NSLog(@"%p", pInt);
+  
+  pInt = malloc(sizeof(int));
+  
+  *pInt = 10;
+  NSLog(@"%@", @(*pInt));
+  
+  free(pInt);
+  
   
   return 0;
 }
