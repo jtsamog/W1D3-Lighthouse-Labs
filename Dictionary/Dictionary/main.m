c939b2d9315c970e621fdcd06e474a2676147d93
steve thompson <steven.thompson.ca@gmail.com>
2017-06-27 17:02:59 -0400

    updates dictionary demo

--- Dictionary/Dictionary/main.m
+++ Dictionary/Dictionary/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[]) {
   
   NSDictionary *dict = @{@"name" : @"Bob", @"age" : @22, @"address": @"587 Brink Stree"};
   
-  // loop through a dictionary
+  // loop through a dictionary. why am I casting item to id?
   
   for (id item in dict) {
     NSLog(@"line %d: key: %@, value: %@", __LINE__, item, dict[item]);
@@ -23,7 +23,7 @@ int main(int argc, const char * argv[]) {
   // Immutable dictionaries cannot be changed
   // dict[@"name"] = @"Henry";
   
-  // can't use literal syntax
+  // can't use literal syntax for mutable dictionaries (but there's a trick)
   NSMutableDictionary *mutableDict = [@{@"name" : @"Bob", @"age" : @22, @"address": @"587 Brink Stree"} mutableCopy];
   
   mutableDict[@"name"] = @"Fred";
